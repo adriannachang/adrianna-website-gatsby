977941422fd89cd7097de7d1db89270260ee80cc
Adrianna Chang <chang.adrianna@gmail.com>
2021-12-27 15:17:42 -0500

    Update copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; 2021 Adrianna Chang</p>
+        <p className="copyright">Gatsby Starter - Dimension. Designed by <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
