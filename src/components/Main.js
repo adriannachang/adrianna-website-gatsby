c316915fbd9295590cd6969c34a77e8b29439f5e
Adrianna Chang <chang.adrianna@gmail.com>
2021-12-27 21:10:15 -0500

    Finish contacts section

--- src/components/Main.js
+++ src/components/Main.js
@@ -131,51 +131,36 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <p>
+            You can find me on the following platforms. Come say hi!
+          </p>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/adriannakchang"
                 className="icon fa-twitter"
+                target="_blank"
+                rel="noreferrer"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
+              <a
+                href="https://www.linkedin.com/in/adrianna-chang-42464796"
+                className="icon fa-linkedin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="label">LinkedIn</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/adrianna-chang-shopify"
                 className="icon fa-github"
+                target="_blank"
+                rel="noreferrer"
               >
                 <span className="label">GitHub</span>
               </a>
